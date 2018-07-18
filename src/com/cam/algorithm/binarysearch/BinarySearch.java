671c2cd4ce53787ec9d8716ef0951bdd3e07f18a
Cameron Moore <cmoore@captechventures.com>
2018-07-18 12:51:02 -0400

    Added binarySearch functionality. Still WIP

--- src/com/cam/algorithm/binarysearch/BinarySearch.java
+++ src/com/cam/algorithm/binarysearch/BinarySearch.java
@@ -1,22 +1,43 @@
 package com.cam.algorithm.binarysearch;
 
 public class BinarySearch {
+	// Definition: Search a SORTED array by repeatedly dividing the search interval
+	// in half.
+	// whole array, if less or greater than middle value, go to respective side of
+	// array.
+	// reducing time complexity from O(n) to O(long(n))
+
+	// requirements: Compare x with the middle element
+	// if x matches the middle element, we return the mid index
+	// else if x is great then examine top half
+	// else if x is less than, examine bottom half
+	// recursive implementation
+
+	static int binarySearch(int[] arr, int start, int end, int item) {
+		if (end >= 1) {
+
+			int mid = 1 + (end - 1) / 2;
+			if (item == arr[mid]) {
+				System.out.println("Found it at index location:" + mid);
+				return mid;
+			} else if (item > arr[mid]) {
+				System.out.println("Item is in upper half");
+				return BinarySearch.binarySearch(arr, mid + 1, end, item);
+			} else {
+				System.out.println("Item is in lower half");
+				return BinarySearch.binarySearch(arr, 0, mid - 1, item);
+			}
+
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] testArr = { 1, 2, 5, 6, 7, 8, 11, 12, 13, 14, 15 };
+		int end = testArr.length;
 
-	}
-	
-	//method is recursive
-	
-	public int binarySearch(int current, int datavalue) {
-		if(current==datavalue) {
-			return current;
-		}
-		
-		
-		
-		return 1;
+		BinarySearch.binarySearch(testArr, 0, end-1, 6);
+
 	}
 
 }
